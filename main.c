d0a3cc88996a8d726eec02a5bd8733465b95521f
Simon Newton <nomis52@gmail.com>
2014-12-21 16:33:30 -0800

    Enable sending a entire buffer of data.

--- main.c
+++ main.c
@@ -26,6 +26,10 @@
 #define DMX_FREQ     250000
 #define DMX_UART UART1
 
+#define ARRAYSIZE(a) \
+  ((sizeof(a) / sizeof(*(a))) / \
+  (!(sizeof(a) % sizeof(*(a)))))
+
 // Config, this makes the clock run at 80Mhz.
 //---------------------
 
@@ -35,58 +39,71 @@
 
 volatile bool finished_tx = false;
 
-/*
+// Null start code.
 const uint8_t data[] = {
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
   11, 12, 13, 14, 15, 16, 17, 18,
   19, 20, 21, 22, 23, 24, 25, 26
 };
 
-struct TxBuffer {
-  const uint8_t *start;
+typedef struct {
+  // The next byte to send
+  const uint8_t *ptr;
+  // The last byte to send
   const uint8_t *end;
-  uint8_t offset;
+} TxBuffer;
+
+typedef enum {
+  BEGIN_SEND,
+  SENDING,
+  SLEEPING,
+} AppState;
+
+TxBuffer tx = {
+  .ptr = NULL,
+  .end = NULL,
 };
- */
+
+AppState app_state = BEGIN_SEND;
 
 void sleep() {
   int i = 1024 * 1024;
   while (i--);
 }
 
-void sendByteAndWait(uint8_t byte) {
-  mPORTDSetBits(BIT_0);
-  UARTSendDataByte(DMX_UART, byte);
-  while (!UARTTransmissionHasCompleted(DMX_UART));
-  mPORTDClearBits(BIT_0);
+void sendBytes() {
+  while (U1STAbits.UTXBF == 0 && tx.ptr <= tx.end) {
+    UARTSendDataByte(DMX_UART, *(tx.ptr++));
+  }
 }
 
-void sendBufferAndWaitForInterrupt() {
+void startTx() {
   mPORTDSetBits(BIT_0);
   finished_tx = false;
 
-  UARTSendDataByte(DMX_UART, 1);
-  UARTSendDataByte(DMX_UART, 4);
-  INTEnable(INT_SOURCE_UART_TX(DMX_UART), INT_ENABLED);
-  while (!finished_tx);
-  INTEnable(INT_SOURCE_UART_TX(DMX_UART), INT_DISABLED);
+  sendBytes();
 
-  mPORTDClearBits(BIT_0);
+  INTEnable(INT_SOURCE_UART_TX(DMX_UART), INT_ENABLED);
+}
+
+void resetTxBuffer() {
+  tx.ptr = data;
+  tx.end = data + ARRAYSIZE(data) - 1;
 }
 
 void __ISR(_UART1_VECTOR, ipl6)AdministratorUART1(void) {
   // Is this an RX interrupt?
   if (INTGetFlag(INT_SOURCE_UART_TX(DMX_UART))) {
-    finished_tx = true;
-    // INTClearFlag(INT_SOURCE_UART_TX(DMX_UART));
-    INTEnable(INT_SOURCE_UART_TX(DMX_UART), INT_DISABLED);
+    INTClearFlag(INT_SOURCE_UART_TX(DMX_UART));
+    sendBytes();
+    if (tx.ptr > tx.end) {
+      finished_tx = true;
+      INTEnable(INT_SOURCE_UART_TX(DMX_UART), INT_DISABLED);
+    }
   }
 }
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void Init() {
   // pb_clock is 80Mhz.
   unsigned int pb_clock = SYSTEMConfig(SYS_FREQ, SYS_CFG_ALL);
 
@@ -114,21 +131,32 @@ int main(int argc, char** argv) {
   INTEnableInterrupts();
 
   UARTEnable(DMX_UART, UART_ENABLE_FLAGS(UART_PERIPHERAL | UART_TX));
+}
 
-  // Initialization complete
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  Init();
+
   while (1) {
-    mPORTDClearBits(BIT_2 | BIT_1 | BIT_0);
-
-    //    sendByteAndWait(0x11);
-    //    sendByteAndWait(0x55);
-    //    sendByteAndWait(0xaa);
-    //    sendByteAndWait(0x10);
-    sendBufferAndWaitForInterrupt();
-
-
-    // Insert some delay
-    sleep();
+    switch (app_state) {
+      case BEGIN_SEND:
+        resetTxBuffer();
+        startTx();
+        app_state = SENDING;
+        break;
+      case SENDING:
+        if (finished_tx) {
+          app_state = SLEEPING;
+          mPORTDClearBits(BIT_0);
+        }
+        break;
+      case SLEEPING:
+        sleep();
+        app_state = BEGIN_SEND;
+        break;
+    }
   }
   return (EXIT_SUCCESS);
 }
-
